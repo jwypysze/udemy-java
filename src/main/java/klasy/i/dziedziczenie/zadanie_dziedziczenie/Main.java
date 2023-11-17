7bcd1f5f872b713c411b6453bcaa81b59b50aeff
jwypysze <joanna.wypyszewska@gmail.com>
2023-11-17 17:51:17 +0100

    Klasy i dziedziczenie

---
+++ src/main/java/klasy/i/dziedziczenie/zadanie_dziedziczenie/Main.java
@@ -0,0 +1,11 @@
+package klasy.i.dziedziczenie.zadanie_dziedziczenie;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Pracownik pracownik = new Pracownik("Anna", "Nowak", "kasjerka", "XYZ");
+        pracownik.przedstawSie();
+
+    }
+}
